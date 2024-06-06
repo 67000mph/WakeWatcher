866c69a71b60f042f18a1d0381a44de5dbc73ac7
jgk1111 <jenny.kunz99@hotmail.de>
2024-06-06 11:42:50 +0200

    Kniebeugen hinzugefügt

--- motion_detection.py
+++ motion_detection.py
@@ -78,6 +78,45 @@ def check_push_up(landmarks, status_push_up, cTime, reset_Time_push_up):
         return True, status_push_up, reset_Time_push_up
     return False, status_push_up, reset_Time_push_up
 
+def check_squat(landmarks, status_squat, cTime, reset_Time_squat):
+    if (cTime - reset_Time_squat) > 5:
+        status_squat = 0
+        reset_Time_squat = cTime
+    #Linkes Bein und linke Huefte im Kamerabild
+    if(
+        status_squat == 0
+        and (landmarks[23].y < 1
+        and landmarks[25].y < 1)  
+    ):
+        status_squat = 1
+
+    # Ausgangsposition: Nutzer steht seitlich zur Kamera, Beine gestreckt
+    elif (
+        status_squat == 1 
+        and (landmarks[25].y > landmarks[23].y) 
+    ):
+        status_squat = 2
+        reset_Time_squat = cTime
+
+    # Gesaeß niedriger als Knie und Fueße am Boden
+    elif (
+        status_squat == 2 and 
+        (landmarks[25].y < landmarks[23].y 
+        and landmarks[27].y > landmarks[25].y)
+        
+    ):
+        status_squat = 3
+        reset_Time_squat = cTime
+        
+    # Nutzer in Ausgansposition: Beine gestreckt
+    elif (
+        status_squat == 3 
+        and landmarks[25].y > landmarks[23].y
+    ):
+        status_squat = 0
+        return True, status_squat, reset_Time_squat
+    return False, status_squat, reset_Time_squat
+
 
 def calculate_angle(x1, y1, x2, y2):
     dy = y2 - y1
@@ -126,6 +165,9 @@ def distance_between_points(x1, y1, x2, y2):
 # status_push_up = 0
 # reset_Time_push_up = 0
 # count_push_up = 0
+# status_squat = 0
+# reset_Time_squat = 0
+# count_squat = 0
 
 # if __name__ == "__main__":
 #     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -168,18 +210,30 @@ def distance_between_points(x1, y1, x2, y2):
 #                         count_push_up += 1
 #                         print(f"Anzahl Liegestuetze {count_push_up}")
 
-#             # Draw the pose annotation on the image.
-#             image.flags.writeable = True
-#             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-#             mp_drawing.draw_landmarks(
-#                 image,
-#                 results.pose_landmarks,
-#                 mp_pose.POSE_CONNECTIONS,
-#                 landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
-#             )
-#             # Flip the image horizontally for a selfie-view display.
-#             cv2.imshow("MediaPipe Pose", cv2.flip(image, 1))
-#             if cv2.waitKey(5) & 0xFF == 27:
-#                 break
+            #         #print(f"Status Kniebeugen {status_squat}")
+            #         squat, status_squat, reset_Time_squat = check_squat(
+            #             results.pose_landmarks.landmark,
+            #             status_squat,
+            #             cTime,
+            #             reset_Time_squat
+            #         )
+            #         if squat:
+            #             count_squat += 1
+            #             print(f"Anzahl Kniebeugen {count_squat}")
+
+
+            # # Draw the pose annotation on the image.
+            # image.flags.writeable = True
+            # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # mp_drawing.draw_landmarks(
+            #     image,
+            #     results.pose_landmarks,
+            #     mp_pose.POSE_CONNECTIONS,
+            #     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+            # )
+            # # Flip the image horizontally for a selfie-view display.
+            # cv2.imshow("MediaPipe Pose", cv2.flip(image, 1))
+            # if cv2.waitKey(5) & 0xFF == 27:
+            #     break
 
 #     cap.release()
