be8fcd373a5dd23d12c8f0e5c6a066752941eb32
MarcelFid <marcel.fiderer@web.de>
2024-06-20 11:56:31 +0200

    Alarm sound changed

--- audio.py
+++ audio.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import _play_with_pyaudio
 import threading
 
-ALARM_FILE = "arrow-whoosh.wav"
+ALARM_FILE = "electronic_alarm.wav"
 
 class AudioPlayerThread(threading.Thread):
     def __init__(self, file_path):
